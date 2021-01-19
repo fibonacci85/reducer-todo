ffa19880b29fd905531b26dcba3824113c676330
Francis Bonacic <francisbonacic@icloud.com>
2021-01-18 20:44:50 -0800

    figuring out my state..

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,14 +1,32 @@
-
-import './App.css';
+import React, { useState } from "react";
+import TodoCard from './components/TodoCard';
 
 function App() {
+
+  const [input, setInput] = useState("")
+  const [todoList, setTodoList] = useState(["sample todo"])
+
+  console.log(todoList)
+
+  const newState = [...todoList, input]
+
+  const handleChange = (e) => {
+    setInput(e.target.value)
+  }
+  const handleAddTodoClick = (item) => {
+    item.preventDefault();
+    setTodoList(newState);
+  }
+
   return (
     <div className="App">
       <form>
-        <input type="text" value="temp" ></input>
-        <button>add todo</button>
+        <input type="text" value={input} onChange={handleChange} ></input>
+        <button onClick={handleAddTodoClick}>add todo</button>
         <button>clear todo</button>
       </form>
+
+      <TodoCard todoList={todoList} setTodoList={setTodoList} />
       
     </div>
   );
