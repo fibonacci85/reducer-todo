db55201005ff0719d337570b8f89c98bb39a8ce3
Francis Bonacic <francisbonacic@icloud.com>
2021-01-18 19:31:49 -0800

    setting up reducer

---
+++ todo/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        <input type="text" value="temp" ></input>
+        <button>add todo</button>
+        <button>clear todo</button>
+      </form>
+      
+    </div>
+  );
+}
+
+export default App;
